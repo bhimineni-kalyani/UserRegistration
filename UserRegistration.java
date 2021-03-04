c9dd78962a07e1202b0ee4862e55aa4fb6d21de6
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-03-04 20:28:57 +1400

    Refactor - program2 functionality added

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,5 +1,22 @@
-public class UserRegistration  {
-    public static void main(String[] args){
-        System.out.println("Welcome to User Registration");
+import java.util.regex.*;
+import java.util.Scanner;
+public class UserRegistrationUC2 {
+	private boolean check;
+    private String lastName;
+    Scanner sc = new Scanner(System.in);
+    public void checkLastName() {
+   	    System.out.println("enter last name starts with cap and has minimum 3 character : ");
+   	    lastName = sc.nextLine();
+   	    check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
+   	    if (!check) {
+   		     System.out.println(" last name is invalid ");
+   		     checkLastName();
+   	    }
+   	    else
+   		     System.out.println(" last name is valid ");
+    }
+    public static void main(String args[]) {
+   	     UserRegistrationUC2 user = new UserRegistrationUC2();
+   	     user.checkLastName();
     }
 }
