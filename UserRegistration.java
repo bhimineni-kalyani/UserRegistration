6e3193d01b5003cf4c212b192db56148e38c9fac
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-03-10 23:17:13 +1400

    Refactor - program9 functionality added

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,5 +1,23 @@
-public class UserRegistration  {
-    public static void main(String[] args){
-        System.out.println("Welcome to User Registration");
-    }
+import java.util.Scanner;
+import java.util.regex.Pattern;
+public class Firstname {
+	
+	static   Scanner name = new  Scanner (System.in); 
+	static void Mail() {
+	    String email = name.nextLine();
+	    boolean  user= Pattern.matches("^[A-Za-z0-9+-]+(\\\\.[A-Za-z0-9-]+)*@\" + \"[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,})$", email);
+	    if (user == true ) {
+	      	System.out.println("you entered a valid email  :" +email);
+	    }
+       else { 
+            System.out.println ("Not a valid email");
+			   System.out.println("Enter valid email");
+			   Mail();
+	    }
+	}
+	
+   public static void main(String[] args) {
+	   System.out.println("enter  email id");
+	   Mail();
+   }
 }
